151146c2593b623f7734f0b7149e74ea1e12e6d7
kravninja <tita0@chicagobooth.edu>
2022-10-20 16:58:13 +0000

    Square results

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,10 @@ class ApplicationController < ActionController::Base
 
     render ({:template => "calculation_templates/square_form.html.erb"})
   end
+
+  def calculate_square
+
+    render ({:template => "calculation_templates/square_results.html.erb"})
+  end
+
 end
