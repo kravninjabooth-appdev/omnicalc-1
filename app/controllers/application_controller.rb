d440ceef361c377eb49c8c6790fb3b489928bfb8
kravninja <tita0@chicagobooth.edu>
2022-10-20 19:19:47 +0000

    Square form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,10 @@ class ApplicationController < ActionController::Base
   end
 
   def calculate_square
+    #params = "elephant"=>"42"
 
+      @num = params.fetch("elephant").to_i
+      @square_of_num = @num **2
     render ({:template => "calculation_templates/square_results.html.erb"})
   end
 
